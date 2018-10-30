12ca56bc4191047de1bfe3b585c96a0226c6916d
sogrey <suzhaoheng2006@163.com>
2018-10-30 23:12:20 +0800

    add qrcode

--- flutter_demo/README.md
+++ flutter_demo/README.md
@@ -4,4 +4,5 @@ A new Flutter application.
 
 ![](https://raw.githubusercontent.com/Sogrey/flutter-demos/master/flutter_demo/1.gif)
 
-[apk](https://github.com/Sogrey/flutter-demos/blob/master/flutter_demo/app.apk?raw=true)
+[apk](https://github.com/Sogrey/flutter-demos/blob/master/flutter_demo/apks/app-release.apk?raw=true)
+
--- flutter_demo/lib/second.dart
+++ flutter_demo/lib/second.dart
@@ -1,20 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:qr_flutter/qr_flutter.dart';
+
 class Second extends StatelessWidget {
-  final title ;///页面标题
+  final title;
+
+  ///页面标题
 
   const Second({Key key, this.title}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: new AppBar(
-        title: new Text(title==null?"Second Page":title),
+        title: new Text(title == null ? "Second Page" : title),
       ),
       body: new Center(
-        //onPressed  点击事件
-        child: new RaisedButton(child: new Text("注销"), onPressed: () {
-          //回到上一个页面 该pop将Route从导航器管理的路由栈中移除当前路径
-          Navigator.pop(context);
-        }),
+
+        child: new Column(
+          children: <Widget>[
+            new RaisedButton(child: new Text("注销"), onPressed: () {
+              //回到上一个页面 该pop将Route从导航器管理的路由栈中移除当前路径
+              Navigator.pop(context);
+//          Navigator.pop(context,"携带参数");
+            }),
+
+            new QrImage(
+              data: "1234567890",
+              size: 200.0,
+            ),
+          ],
+        ),
       ),
     );
   }
